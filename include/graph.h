87020bd429c044063c47536f85bc7dc4c10ed6b4
he1kor <helkor.rightway@gmail.com>
2025-05-30 04:56:55 +0300

    feat: Node class

---
+++ include/graph.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+
+class Node{
+    public:
+        Node(int id);
+        Node(int id, std::vector<int> neighbours);
+        void addNeighbour(int id);
+        void removeNeighbour(int id);
+        const std::vector<int>& getNeighbours();
+        int getID();
+    private:
+        int id;
+        std::vector<int> neighbours;
+}
---
+++ src/graph.cpp
@@ -0,0 +1,22 @@
+#include "graph.h"
+
+Node::Node(int id){
+    this->id = id;
+}
+
+Node::Node(int id, std::vector<int> neighbours){
+    this->id = id;
+    this->neighbours = neighbours;
+}
+
+void Node::addNeighbour(int id){
+    neighbours.push_back(id);
+}
+
+void Node::removeNeighbour(int id){
+    neighbours.erase(std::remove(neighbours.begin(), neighbours.end(), id), neighbours.end());
+}
+
+const std::vector<int> &Node::getNeighbours(){
+    return neighbours;
+}
