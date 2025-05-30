1ff22cd1c8bb730f75650b34a7d0e63a9e1d5681
he1kor <helkor.rightway@gmail.com>
2025-05-30 05:21:43 +0300

    change: neighbours (vector -> set)

--- include/graph.h
+++ include/graph.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <set>
 
 class Node{
     public:
@@ -7,9 +8,19 @@ class Node{
         Node(int id, std::vector<int> neighbours);
         void addNeighbour(int id);
         void removeNeighbour(int id);
-        const std::vector<int>& getNeighbours();
+        const std::set<int>& getNeighbours();
         int getID();
     private:
         int id;
-        std::vector<int> neighbours;
+        std::set<int> neighbours;
+};
+
+class Graph{
+    public:
+        Graph();
+        Graph(std::vector<Node> nodes);
+        void insertNode(Node node);
+    private:
+        //indexed by ID
+        std::vector<Node> nodes;
 };
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,5 +1,6 @@
 #include "graph.h"
 #include <algorithm>
+#include <stdexcept>
 
 Node::Node(int id){
     this->id = id;
@@ -7,17 +8,28 @@ Node::Node(int id){
 
 Node::Node(int id, std::vector<int> neighbours){
     this->id = id;
-    this->neighbours = neighbours;
+    this->neighbours = std::set(neighbours.begin(), neighbours.end());
 }
 
 void Node::addNeighbour(int id){
-    neighbours.push_back(id);
+    neighbours.insert(id);
 }
 
 void Node::removeNeighbour(int id){
     neighbours.erase(std::remove(neighbours.begin(), neighbours.end(), id), neighbours.end());
 }
 
-const std::vector<int> &Node::getNeighbours(){
+const std::set<int> &Node::getNeighbours(){
     return neighbours;
+}
+
+Graph::Graph(){}
+
+Graph::Graph(std::vector<Node> nodes){
+    this->nodes.reserve(nodes.size());
+    for (auto node : nodes){
+        if (node.getID() > nodes.size() || node.getID() < 0)
+            throw std::runtime_error("The Node IDs are not conconsecutive (given Node ID is out of bounds)");
+        nodes[node.getID()] = node;
+    }
 }
