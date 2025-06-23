bbc75b518b328e9c77cc2a0ff5f6654d6c1b9792
he1kor <helkor.rightway@gmail.com>
2025-06-23 18:03:03 +0300

    fix: edgeIDs hash function

--- include/edge_graph.h
+++ include/edge_graph.h
@@ -1,6 +1,16 @@
 #include "graph.h"
 #include <unordered_map>
 
+#include <functional>
+
+struct PairHash {
+    std::size_t operator()(const std::pair<int, int>& e) const {
+        auto a = std::min(e.first, e.second);
+        auto b = std::max(e.first, e.second);
+        return std::hash<int>{}(a) ^ (std::hash<int>{}(b) << 1);
+    }
+};
+
 class EdgeGraph : public Graph{
     public:
         EdgeGraph();
@@ -10,6 +20,6 @@ class EdgeGraph : public Graph{
         void removeEdge(int node1, int node2) override;
         int getEdgeID(int node1, int node2) const;
     private:
-        std::unordered_map<std::pair<int, int>, int> edgeIDs;
+        std::unordered_map<std::pair<int, int>, int, PairHash> edgeIDs;
         void initializeEdgeIDs();
 };
