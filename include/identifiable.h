a2f0bb8f919f9de0d4956b164d4d48403918b323
He1KoR <helkor.rightway@gmail.com>
2025-07-09 12:19:44 +0300

    feat: Identifiable hash functions

--- include/identifiable.h
+++ include/identifiable.h
@@ -1,9 +1,25 @@
 #pragma once
+#include <functional>
+
 
 class Identifiable{
     protected:
         int id;
     public:
         virtual ~Identifiable() = default;
-        int getID();
+        int getID() const;
+};
+
+struct IDHash{
+    std::size_t operator()(const Identifiable& e) const {
+        return std::hash<int>{}(e.getID());
+    }
+};
+
+struct PairIDHash {
+    std::size_t operator()(const std::pair<Identifiable, Identifiable>& e) const {
+        auto a = std::min(e.first.getID(), e.second.getID());
+        auto b = std::max(e.first.getID(), e.second.getID());
+        return std::hash<int>{}(a) ^ (std::hash<int>{}(b) << 1);
+    }
 };
