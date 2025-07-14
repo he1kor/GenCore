00d8321de5cb389d4e1533f7fc46670f4f08b43e
he1kor <helkor.rightway@gmail.com>
2025-07-15 02:25:51 +0300

    feat: Grid

---
+++ include/grid.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <vector>
+#include <unordered_map>
+
+#include "identifiable.h"
+
+template<typename T>
+class Grid{
+    static_assert(std::is_base_of_v<Identifiable, T>, "T must inherit from Identifiable");
+    public:
+        Grid(int width, int height);
+
+        template<std::ranges::input_range R>
+        requires std::convertible_to<std::ranges::range_value_t<R>, T>
+        Grid(int width, int height, R&& range);
+
+        void setTile(int x, int y, Identifiable value);
+        Identifiable getTile(int x, int y);
+        int getWidth();
+        int getHeight();
+    private:
+        int width;
+        int height;
+        std::vector<std::vector<Identifiable>> matrix;
+        std::unordered_map<Identifiable, T, IDHash> tileset;
+};
+
+template <typename T> 
+Grid<T>::Grid(int width, int height) : width(width), height(height){
+    matrix = std::vector<std::vector<Identifiable>>(height, std::vector(width));
+}
+
+template <typename T>
+void Grid<T>::setTile(int x, int y, Identifiable value){
+    matrix.at(y).at(x) = value;
+}
+
+template <typename T>
+Identifiable Grid<T>::getTile(int x, int y){
+    return matrix.at(y).at(x);
+}
+
+template <typename T>
+int Grid<T>::getWidth(){
+    return width;
+}
+
+template <typename T>
+int Grid<T>::getHeight(){
+    return height;
+}
+
+template <typename T> 
+template <std::ranges::input_range R>
+requires std::convertible_to<std::ranges::range_value_t<R>, T>
+Grid<T>::Grid(int width, int height, R&& range) : Grid(width, height){
+    for (const T& t : range) {
+        tileset[static_cast<Identifiable>(t)] = t;
+    }
+}
