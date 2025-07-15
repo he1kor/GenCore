7a30a1c98e714c610d62b63cd32776aa96c7cde0
he1kor <helkor.rightway@gmail.com>
2025-07-15 21:25:38 +0300

    feat: status callbacks

--- include/simulator.h
+++ include/simulator.h
@@ -9,12 +9,18 @@ enum class SimulationStatus{
 class Simulator{
     public:
         Simulator();
-        virtual void start();
-        virtual void step();
-        virtual void finish();
-        virtual void reset();
-        int getStep();
-        SimulationStatus getStatus();
+        virtual void start() final;
+        virtual void step() final;
+        virtual void finish() final;
+        virtual void reset() final;
+        virtual void onStart(){};
+        virtual void onStep() = 0;
+        virtual void onFinish(){};
+        virtual void onReset(){};
+
+
+        int getStep() const;
+        SimulationStatus getStatus() const;
     private:
         int steps = -1;
         SimulationStatus status = SimulationStatus::INIT;
--- src/simulator.cpp
+++ src/simulator.cpp
@@ -6,34 +6,39 @@ Simulator::Simulator(){};
 
 void Simulator::start(){
     if (status != SimulationStatus::INIT){
-        throw std::logic_error("Can't start, the simulation is not initialized!");
+        throw std::logic_error("Can't start, the status is not INIT!");
     }
-    steps = 0;
     status = SimulationStatus::RUNNING;
+    steps = 0;
+    onStart();
 }
 
 void Simulator::step(){
     if (status != SimulationStatus::RUNNING)
-        throw std::logic_error("Can't step, the simulation is not running!");
+        throw std::logic_error("Can't step, the status is not RUNNING!");
     steps++;
+    onStep();
 }
 
 void Simulator::finish(){
     if (status != SimulationStatus::RUNNING)
-        throw std::logic_error("Can't finish, the simulation is not running!");
+        throw std::logic_error("Can't finish, the status is not RUNNING!");
     status = SimulationStatus::STOPPED;
+    onFinish();
 }
 
 void Simulator::reset(){
     if (status != SimulationStatus::STOPPED)
-        throw std::logic_error("Can't reset, the simulation is not finished!");
+        throw std::logic_error("Can't reset, the status is not STOPPED!");
     status = SimulationStatus::INIT;
+    steps = -1;
+    onReset();
 }
 
-int Simulator::getStep(){
+int Simulator::getStep() const{
     return steps;
 }
 
-SimulationStatus Simulator::getStatus(){
+SimulationStatus Simulator::getStatus() const{
     return status;
 }
