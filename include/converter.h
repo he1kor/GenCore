b7fa08eb9e14cc9d0c30e503d8fe14964485c7c7
he1kor <helkor.rightway@gmail.com>
2025-07-15 16:22:07 +0300

    feat: doubleToIndex

--- include/converter.h
+++ include/converter.h
@@ -2,6 +2,19 @@
 
 #include "plane.h"
 #include "grid.h"
+#include <stdexcept>
+
+#include <cmath>
+
+int doubleToIndex(double value, double size) {
+    if (size < 0.0)
+        throw std::invalid_argument("size must be non-negative");
+    if (value >= size)
+        return std::ceil(size) - 1;;
+    if (value < 0.0)
+        return 0;
+    return static_cast<int>(std::floor(value));
+}
 
 template<typename T>
 Grid<T> safeRasterizePlane(const Plane<T>& plane){
@@ -9,6 +22,7 @@ Grid<T> safeRasterizePlane(const Plane<T>& plane){
     tileset.reserve(plane.getSpotsNumber());
     for (Identifiable id : plane.getIDs()){
         tileset.push_back(plane.getValue(id));
+        DoublePoint2 point = plane.getRelativePosition(id);
     }
     Grid<T> result(ceil(plane.getWidth()), ceil(plane.getHeight()), tileset);
 };
