68277f7aba52e3f371ab8b4b8e1bc5a998afaa4f
he1kor <helkor.rightway@gmail.com>
2025-07-19 20:56:08 +0300

    feat: magnetic

---
+++ include/magnetic.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Magnetic{
+    public:
+        Magnetic();
+        constexpr Magnetic(double val) : susceptibility(val){}
+        void setSuspectibility(double val);
+        double getSuspectibility() const;
+        bool isAttractive() const;
+        bool isRepulsive() const;
+        bool isNotMagnetic() const;
+        bool isMagnetic() const;
+        void reversePole();
+    private:
+        double susceptibility = 0;
+};
---
+++ src/magnetic.cpp
@@ -0,0 +1,33 @@
+#include "magnetic.h"
+
+double Magnetic::getSuspectibility() const{
+    return susceptibility;
+}
+
+bool Magnetic::isAttractive() const{
+    return getSuspectibility() > 0;
+}
+
+bool Magnetic::isRepulsive() const{
+    return getSuspectibility() < 0;
+}
+
+bool Magnetic::isNotMagnetic() const{
+    return getSuspectibility() == 0;
+}
+
+bool Magnetic::isMagnetic() const{
+    return !isNotMagnetic();
+}
+
+void Magnetic::reversePole(){
+    setSuspectibility(-getSuspectibility());
+}
+
+Magnetic::Magnetic(){}
+
+Magnetic::Magnetic(double susceptibility) : susceptibility{susceptibility}{}
+
+void Magnetic::setSuspectibility(double val){
+    this->susceptibility = val;
+}
