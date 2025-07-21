bebe485ab33f865f407a15b7889ffad4ffa05cf4
he1kor <helkor.rightway@gmail.com>
2025-07-21 18:58:10 +0300

    feat: SafeDoubleVector with virtual getters/setters

--- include/2d.h
+++ include/2d.h
@@ -1,8 +1,19 @@
 #pragma once
 
 struct DoubleVector2 {
+    DoubleVector2(double x = 0, double y = 0) : x(x), y(y) {}
     double x = 0;
     double y = 0;
+    DoubleVector2 operator*(double scalar) const;
+};
+
+struct SafeDoubleVector2 : protected DoubleVector2{
+    SafeDoubleVector2(double x = 0, double y = 0) : DoubleVector2{x, y}{};
+    SafeDoubleVector2(const DoubleVector2& another) : DoubleVector2{another.x, another.y}{};
+    virtual double getX() const{return x;};
+    virtual double getY() const{return y;};
+    virtual void setX(double x){this->x = x;};
+    virtual void setY(double y){this->y = y;};
 };
 
 struct IntVector2 {
