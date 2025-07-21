44fc64f8710012998a5a643e2964cd5a98767467
he1kor <helkor.rightway@gmail.com>
2025-07-21 19:03:57 +0300

    feat: magnet

--- include/magnet.h
+++ include/magnet.h
@@ -5,14 +5,20 @@
 #include "magnetic.h"
 #include "2d.h"
 #include <functional>
+#include <cmath>
 
 template <typename T>
-concept IsMagneticPoint = 
-    std::is_base_of_v<DoubleVector2, T> &&
+concept IsMagneticPoint =
     std::is_base_of_v<Magnetic, T>;
 
-class Magnet : private DoubleVector2{
+
+class Magnet : public SafeDoubleVector2{
     public:
+        static constexpr auto linearDistanceToForce = [](double distance) {
+            constexpr double maxDistance = 5.0;
+            distance = std::min(maxDistance, distance);
+            return 1;
+        };
         static constexpr auto hyperbolicDistanceToForce = [](double distance) {
             return 1.0 / (distance);
         };
@@ -25,23 +31,25 @@ class Magnet : private DoubleVector2{
         Magnet(DoubleVector2 point2, double force);
         void setPos(DoubleVector2 point2);
 
-        DoubleVector2 getRepulse(DoubleVector2 object);
-        DoubleVector2 getAttract(DoubleVector2 object);
+        DoubleVector2 calculateForce(const DoubleVector2& object) const;
         
         template <IsMagneticPoint MagneticPointT>
-        DoubleVector2 getForce(MagneticPointT object);
+        DoubleVector2 calculateForce(const DoubleVector2& object, const MagneticPointT& magnetic) const;
+
+        double getForce() const{return force;};
         
         void setForceFunction(std::function<double(double)> distanceToForce);
-        
+    protected:
+        Magnet() : force(0){};
     private:
-        DoubleVector2 getForce(DoubleVector2 object, double force);
+        DoubleVector2 calculateForce(const DoubleVector2& object, double force) const;
         static constexpr double defaultRepulseForce = 1.0;
         static constexpr double defaultAttractForce = -1.0;
-        std::function<double(double)> distanceToForce = hyperbolicDistanceToForce;
+        std::function<double(double)> distanceToForce = linearDistanceToForce;
         double force;
 };
 
 template <IsMagneticPoint MagneticPointT>
-DoubleVector2 Magnet::getForce(MagneticPointT object){
-    return getForce(static_cast<DoubleVector2>(object), object.getSuspectibility());
+DoubleVector2 Magnet::calculateForce(const DoubleVector2& object, const MagneticPointT& magnetic) const{
+    return calculateForce(static_cast<DoubleVector2>(object), magnetic.getSuspectibility());
 }
--- src/magnet.cpp
+++ src/magnet.cpp
@@ -2,34 +2,29 @@
 
 #include <cmath>
 
-Magnet::Magnet(double force) : DoubleVector2{0, 0}{}
+Magnet::Magnet(double force) : SafeDoubleVector2{0.0, 0.0}, force{force}{}
 
-Magnet::Magnet(double x, double y, double force) : DoubleVector2{x, y}, force{force} {}
+Magnet::Magnet(double x, double y, double force) : SafeDoubleVector2{x, y}, force{force} {}
 
-Magnet::Magnet(DoubleVector2 point2, double force) : DoubleVector2{point2}, force{force} {}
-
-DoubleVector2 Magnet::getRepulse(DoubleVector2 object){
-    return getForce(object, Magnet::defaultRepulseForce);
-}
-
-DoubleVector2 Magnet::getAttract(DoubleVector2 object){
-    return getForce(object, Magnet::defaultAttractForce);
-}
+Magnet::Magnet(DoubleVector2 point2, double force) : SafeDoubleVector2{point2}, force{force} {}
 
 void Magnet::setPos(DoubleVector2 point2){
     this->x = point2.x;
     this->y = point2.x;
 }
 
+DoubleVector2 Magnet::calculateForce(const DoubleVector2& object) const{
+    return calculateForce(object, Magnet::defaultRepulseForce);
+}
 
 void Magnet::setForceFunction(std::function<double(double)> distanceToForce){
     this->distanceToForce = distanceToForce;
 }
 
-DoubleVector2 Magnet::getForce(DoubleVector2 object, double susceptibility){
+DoubleVector2 Magnet::calculateForce(const DoubleVector2& object, double susceptibility) const{
     double dx = object.x - x;
     double dy = object.y - y;
     double distance = std::sqrt(dx * dx + dy * dy);
-    double force = susceptibility * distanceToForce(distance);
-    return {dx * force, dy * force};
+    double appliedForce = force * susceptibility * distanceToForce(distance);
+    return {dx * appliedForce, dy * appliedForce};
 }
