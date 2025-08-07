22e0c273c71f4770a309b946de23ad17cba09c97
he1kor <helkor.rightway@gmail.com>
2025-08-07 03:11:12 +0300

    wrong init fix, feat: edgeIDs, edgeToNodes map

--- include/edge_graph.h
+++ include/edge_graph.h
@@ -5,12 +5,14 @@
 
 #include <functional>
 
-template<typename NodeT, typename EdgeT>
+template<typename NodeT, typename EdgeT = Identifiable>
 class EdgeGraph : public Graph<NodeT>{
     static_assert(std::is_base_of_v<Identifiable, NodeT>, "NodeT must inherit from Identifiable");
     static_assert(std::is_base_of_v<Identifiable, EdgeT>, "EdgeT must inherit from Identifiable");
     public:
         EdgeGraph();
+        EdgeGraph(const std::vector<Node<NodeT>>& nodes);
+        EdgeGraph(const std::vector<std::pair<NodeT, std::vector<Identifiable>>>& rawGraph);
         EdgeGraph(
             const std::vector<std::pair<NodeT, std::vector<Identifiable>>>& rawGraph,
             const std::unordered_map<std::pair<Identifiable, Identifiable>, EdgeT, PairIDHash>& edges
@@ -25,13 +27,16 @@ class EdgeGraph : public Graph<NodeT>{
         Identifiable getEdgeID(Identifiable node1, Identifiable node2) const;
         const EdgeT& getEdge(Identifiable edgeID) const;
         const EdgeT& getEdge(Identifiable node1, Identifiable node2) const;
-        EdgeGraph(const std::vector<std::pair<NodeT, std::vector<Identifiable>>>& rawGraph);
-        EdgeGraph(const std::vector<Node<NodeT>>& nodes);
+        std::pair<Identifiable, Identifiable> getEdgeNodeIDs(Identifiable edgeID) const;
+        std::pair<NodeT, NodeT> getEdgeNodes(Identifiable edgeID) const;
+        const std::vector<Identifiable>& getEdgeIDs() const;
     protected:
     private:
-        std::unordered_map<std::pair<Identifiable, Identifiable>, Identifiable, PairIDHash> edgeIDs;
+        std::unordered_map<std::pair<Identifiable, Identifiable>, Identifiable, PairIDHash> nodesToEdge;
+        std::unordered_map<Identifiable, std::pair<Identifiable, Identifiable>, IDHash> edgeToNodes;
         std::unordered_map<Identifiable, EdgeT, IDHash> edges;
-        void initializeEdgeIDs();
+        std::vector<Identifiable> edgeIDs;
+        void initializeEmptyEdges();
 };
 
 
@@ -40,8 +45,8 @@ template<typename NodeT, typename EdgeT>
 EdgeGraph<NodeT, EdgeT>::EdgeGraph(){}
 
 template<typename NodeT, typename EdgeT>
-void EdgeGraph<NodeT, EdgeT>::initializeEdgeIDs(){
-    if (edgeIDs.size() != 0){
+void EdgeGraph<NodeT, EdgeT>::initializeEmptyEdges(){
+    if (nodesToEdge.size() != 0){
         throw std::logic_error("Edges already initialized.");
     }
     int max_edge_id = 0;
@@ -49,13 +54,15 @@ void EdgeGraph<NodeT, EdgeT>::initializeEdgeIDs(){
         auto node1 = this->getNode(id);
         for (Identifiable id2 : node1.getNeighbours()){
             const Node<NodeT>& node2 = this->getNode(id2);
-            if (edgeIDs.count(std::make_pair(node2.getID(), node1.getID()))){
-                edgeIDs[std::make_pair(node1.getID(), node2.getID())] = edgeIDs.at(std::make_pair(node2.getID(), node1.getID()));
+            if (nodesToEdge.count(std::make_pair(node2.getID(), node1.getID()))){
+                nodesToEdge[std::make_pair(node1.getID(), node2.getID())] = nodesToEdge.at(std::make_pair(node2.getID(), node1.getID()));
                 continue;
             }
-            edgeIDs[std::make_pair(node1.getID(), node2.getID())] = max_edge_id;
+            edgeToNodes[max_edge_id] = std::make_pair(node1.getID(), node2.getID());
+            nodesToEdge[std::make_pair(node1.getID(), node2.getID())] = max_edge_id;
 
             edges[max_edge_id] = EdgeT();
+            edgeIDs.push_back(static_cast<Identifiable>(max_edge_id));
             max_edge_id++;
         }
     }
@@ -63,48 +70,63 @@ void EdgeGraph<NodeT, EdgeT>::initializeEdgeIDs(){
 
 template<typename NodeT, typename EdgeT>
 EdgeGraph<NodeT, EdgeT>::EdgeGraph(
-    const std::vector<std::pair<NodeT, std::vector<Identifiable>>>& rawGraph) : Graph<NodeT>(rawGraph){
-        initializeEdgeIDs();
-    }
+    const std::vector<std::pair<NodeT, std::vector<Identifiable>>>& rawGraph)
+        : Graph<NodeT>(rawGraph){
+            initializeEmptyEdges();
+        }
 
+template<typename NodeT, typename EdgeT>
+EdgeGraph<NodeT, EdgeT>::EdgeGraph(
+    const std::vector<Node<NodeT>>& nodes)
+        : Graph<NodeT>(nodes){
+            initializeEmptyEdges();
+        }
+    
 template <typename NodeT, typename EdgeT>
 EdgeGraph<NodeT, EdgeT>::EdgeGraph(
-    const std::vector<std::pair<NodeT, std::vector<Identifiable>>> &rawGraph,
-    const std::unordered_map<std::pair<Identifiable, Identifiable>, EdgeT, PairIDHash>& edges) : EdgeGraph<NodeT, EdgeT>(rawGraph){
-    initializeEdges(edges);
-}
+    const std::vector<std::pair<NodeT, std::vector<Identifiable>>>& rawGraph,
+    const std::unordered_map<std::pair<Identifiable, Identifiable>, EdgeT, PairIDHash>& edges)
+        : Graph<NodeT>(rawGraph){
+            initializeEdges(edges);
+        }
 
 template <typename NodeT, typename EdgeT>
 EdgeGraph<NodeT, EdgeT>::EdgeGraph(
     const std::vector<Node<NodeT>>& nodes,
-    const std::unordered_map<std::pair<Identifiable, Identifiable>, EdgeT, PairIDHash>& edges) : EdgeGraph<NodeT, EdgeT>(nodes){
-        initializeEdges(edges);
-    }
+    const std::unordered_map<std::pair<Identifiable, Identifiable>, EdgeT, PairIDHash>& edges)
+        : Graph<NodeT>(nodes){
+            initializeEdges(edges);
+        }
 
 template <typename NodeT, typename EdgeT>
 void EdgeGraph<NodeT, EdgeT>::initializeEdges(
-    const std::unordered_map<std::pair<Identifiable, Identifiable>, EdgeT, PairIDHash> &edges){
-    for (const auto& [nodePair, edge] : edges){
-        edgeIDs[nodePair] = edge;
-        edges[static_cast<Identifiable>(edge)] = edge;
+    const std::unordered_map<std::pair<Identifiable, Identifiable>, EdgeT, PairIDHash>& edges){
+        nodesToEdge.reserve(edges.size() * 2);
+        edgeToNodes.reserve(edges.size());
+        edges.reserve(edges.size());
+        edgeIDs.reserve(edges.size());
+        for (const auto& [nodePair, edge] : edges){
+            nodesToEdge[nodePair] = static_cast<Identifiable>(edge);
+            nodesToEdge[std::make_pair(nodePair.second, nodePair.first)] = static_cast<Identifiable>(edge); //TODO:: make nodesToEdge store only N pairs instead of 2*N with reversed pairs;
+            edgeToNodes[static_cast<Identifiable>(edge)] = nodePair;
+            edges[static_cast<Identifiable>(edge)] = edge;
+            edgeIDs.push_back(static_cast<Identifiable>(edge));
+        }
     }
-}
-
-template<typename NodeT, typename EdgeT>
-EdgeGraph<NodeT, EdgeT>::EdgeGraph(const std::vector<Node<NodeT>>& nodes) : Graph<NodeT>(nodes){
-    initializeEdgeIDs();
-}
 
 template<typename NodeT, typename EdgeT>
 void EdgeGraph<NodeT, EdgeT>::removeEdge(Identifiable node1, Identifiable node2){
-    this->removeEdge(node1, node2);
-    edgeIDs.erase({node1, node2});
-    edgeIDs.erase({node2, node1});
+    Graph<NodeT>::removeEdge(node1, node2);
+    
+    edgeToNodes.erase(nodesToEdge.at({node1, node2}));
+
+    nodesToEdge.erase({node1, node2});
+    nodesToEdge.erase({node2, node1});
 }
 
 template<typename NodeT, typename EdgeT>
 Identifiable EdgeGraph<NodeT, EdgeT>::getEdgeID(Identifiable node1, Identifiable node2) const{
-    return edgeIDs.at({node1, node2});
+    return nodesToEdge.at({node1, node2});
 }
 
 template <typename NodeT, typename EdgeT>
@@ -118,10 +140,28 @@ const EdgeT &EdgeGraph<NodeT, EdgeT>::getEdge(Identifiable node1, Identifiable n
 }
 
 template <typename NodeT, typename EdgeT>
+std::pair<Identifiable, Identifiable> EdgeGraph<NodeT, EdgeT>::getEdgeNodeIDs(Identifiable edgeID) const{
+    return edgeToNodes.at(edgeID);
+}
+
+template <typename NodeT, typename EdgeT>
+std::pair<NodeT, NodeT> EdgeGraph<NodeT, EdgeT>::getEdgeNodes(Identifiable edgeID) const{
+    auto [id1, id2] = getEdgeNodeIDs(edgeID);
+    return {this->getNode(id1), this->getNode(id2)};
+}
+
+template <typename NodeT, typename EdgeT>
+const std::vector<Identifiable> &EdgeGraph<NodeT, EdgeT>::getEdgeIDs() const{
+    return edgeIDs;
+}
+
+template <typename NodeT, typename EdgeT>
 void EdgeGraph<NodeT, EdgeT>::separate(Identifiable id){
     for (Identifiable neighbourID : this->getNeighbours(id)){
-        edgeIDs.erase({neighbourID, id});
-        edgeIDs.erase({id, neighbourID});
+        edgeToNodes.erase(nodesToEdge.at({id, neighbourID}));
+
+        nodesToEdge.erase({neighbourID, id});
+        nodesToEdge.erase({id, neighbourID});
     }
     this->separate(id);
 }
